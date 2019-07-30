d6c2b3bd6142631b23174aad67104f5129915356
changxianglin <281532612@qq.com>
2019-07-30 23:33:57 +0800

    start write page

--- page/index/index.js
+++ page/index/index.js
@@ -1,1 +1,11 @@
-console.log(1)
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Provider } from 'react-redux'
+
+import Main from './Main/Main.jsx'
+
+ReactDOM.render(
+  <Provider store = {store}><Main /></Provider>,
+  document.getElementById('root')
+)
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const fs = require('fs')
 const srcRoot = path.resolve(__dirname, './src')
 const devPath = path.resolve(__dirname, 'dev')
@@ -31,9 +32,13 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.(js|jsx)$/, use: [{loader: 'babel-loader'}], include: srcRoot},
       { test: /\.css$/, use: ['style-loader', 'css-loader'], include: srcRoot},
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'], include: srcRoot},
       { test: /\.(png|jpg|jpeg)$/, use: 'url-loader?limit=8192', include: srcRoot}
     ]
-  }
+  },
+  plugin: [
+    new HtmlWebpackPlugin()
+  ]
 }
