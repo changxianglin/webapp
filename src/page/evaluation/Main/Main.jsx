b5aadbc2d65cdcaeec96b878ac66f886457d81df
changxianglin <281532612@qq.com>
2019-09-13 00:22:41 +0800

    write css finish

--- src/page/evaluation/Main/Main.jsx
+++ src/page/evaluation/Main/Main.jsx
@@ -18,19 +18,12 @@ class Main extends React.Component {
       array.push(<div key = {i} className = {cls}></div>)
     }
 
-    return array
-  }
-
-  render() {
-    return (
-      <div className = 'content'>
-        <NavHeader title = '评价' />
-        <div className = 'eva-content'>
-          <div className = 'star-area'>
+    return array } render() { return ( <div className = 'content'> <NavHeader title = '评价' /> <div className = 'eva-content'><div className = 'star-area'>
             {this.renderStar()}
           </div>
           <div className = 'comment'>
             <textarea maxLength = '140' placeholder = '亲,菜品的味道如何，商机网少的发舒服点.' className = 'comment-input'></textarea>
+            <span className = 'count'>32</span>
           </div>
           <p className = 'one-line product-name'>+1243哦啊设计的佛啊惊世毒妃</p>
         </div>
