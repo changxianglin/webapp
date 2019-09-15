2af664dec45a61ce05519396eccb0b5026c8c3ea
changxianglin <281532612@qq.com>
2019-09-15 23:59:28 +0800

    add event

--- src/page/evaluation/Main/Main.jsx
+++ src/page/evaluation/Main/Main.jsx
@@ -13,23 +13,50 @@ class Main extends React.Component {
 
     this.state = {
       count: this.maxCount,
+      startIndex: 0,
     }
+  }
+
+  componentDidMount() {
+    this.refs.commentInput.addEventListener('compositionstart', () => {
+      this.chineseInputing = true
+    })
+
+    this.refs.commentInput.addEventListener('compositionend', (e) => {
+      this.chineseInputing = false
+      this.onInput(e.target.value)
+    })
+  }
+
+  onInput(value) {
+    let num = value.length
+    if(!this.chineseInputing) {
+      this.setState({
+        count: this.maxCount - num
+      })
+    }
+  }
+
+  doEva(i) {
+    this.setState({
+      startIndex: i + 1,
+    })
   }
 
   renderStar() {
     let array = []
 
     for(let i = 0; i < 5; i++) {
-      let cls = 'star-item'
-      array.push(<div key = {i} className = {cls}></div>)
+      let cls = i >= this.state.startIndex ?  'star-item' : 'start-item light'
+      array.push(<div onClick = {() => this.doEva(i)} key = {i} className = {cls}></div>)
     }
 
     return array } render() { return ( <div className = 'content'> <NavHeader title = '评价' /> <div className = 'eva-content'><div className = 'star-area'>
             {this.renderStar()}
           </div>
           <div className = 'comment'>
-            <textarea maxLength = '140' placeholder = '亲,菜品的味道如何，商机网少的发舒服点.' className = 'comment-input'></textarea>
-            <span className = 'count'>32</span>
+            <textarea ref = "commentInput" onChange = {(e) => this.onInput(e.target.value)} maxLength = '140' placeholder = '亲,菜品的味道如何，商机网少的发舒服点.' className = 'comment-input'></textarea>
+            <span className = 'count'>{this.state.count}</span>
           </div>
           <p className = 'one-line product-name'>+1243哦啊设计的佛啊惊世毒妃</p>
         </div>
