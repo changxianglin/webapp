b72455464af04a8f917bfa5b0d908d6221381e0c
changxianglin <281532612@qq.com>
2019-10-10 12:42:46 +0800

    component lazyload finish

--- src/page/index/Main/Main.jsx
+++ src/page/index/Main/Main.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Route, withRouter } from 'react-router-dom'
+import Loadable from 'react-loadable';
 
 import BottomBar from '../BottomBar/BottomBar.jsx'
 import Home from '../Home/Home'
-import Order from '../Order/Order'
-import My from '../My/My'
+// import Order from '../Order/Order'
+// import My from '../My/My'
+import Loading from './Loading'
+
+const My = Loadable({
+  loader: () => import(/* webpackChunkName: 'my' */'../My/My'),
+  loading: Loading,
+})
+
+const Order = Loadable({
+  loader: () => import(/* webpackChunkName: 'order' */'../Order/Order'),
+  loading: Loading,
+})
 
 class Main extends Component {
   constructor(props) {
